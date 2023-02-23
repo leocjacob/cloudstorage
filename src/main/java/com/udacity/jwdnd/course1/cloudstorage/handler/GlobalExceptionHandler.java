63e6958e897ad62e68d4afb48262b6787a72ec57
leocjacob <leocjacob@gmail.com>
2023-02-22 22:56:42 -0500

    updated global exception handler

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/handler/GlobalExceptionHandler.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/handler/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
@@ -17,11 +18,24 @@ public class GlobalExceptionHandler {
         Object status = httpServletRequest.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         if (status != null) {
             Integer statusCode = Integer.valueOf(status.toString());
-            model.addAttribute("code",statusCode);
+            model.addAttribute("statusCode",statusCode);
         }else{
-            model.addAttribute("code",404);
+            model.addAttribute("statusCode",404);
         }
         model.addAttribute("message",storageFileNotFoundException.getMessage());
         return "errorx";
     }
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public String handleMaxUploadSizeExceededException(Model model, HttpServletRequest httpServletRequest) {
+        Object status = httpServletRequest.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+            model.addAttribute("statusCode",statusCode);
+        }else{
+            model.addAttribute("statusCode",403);
+        }
+        model.addAttribute("message","File upload error! Please upload file less than 10 MB.");
+        return "errorx";
+    }
+
 }
